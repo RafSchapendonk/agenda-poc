293035f9e5a4b4b41dbabaaa36b6ef2f0af8e8ce
Raf Schapendonk <Rafschapendonk@outlook.com>
2020-03-19 09:43:59 +0100

    the start

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// let today = new Date();
+// let currentMonth = today.getMonth();
+// let currentYear = today.getFullYear();
+// let selectYear = document.getElementById("year");
+// let selectMonth = document.getElementById("month");
+
+// let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// let monthAndYear = document.getElementById("monthAndYear");
+// showCalendar(currentMonth, currentYear);
+bruh

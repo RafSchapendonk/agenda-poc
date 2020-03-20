f2f6950a9121fb4df29e0615a8c703a14882d274
Raf Schapendonk <Rafschapendonk@outlook.com>
2020-03-20 09:45:14 +0100

    push

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
 // let today = new Date();
-// let currentMonth = today.getMonth();
-// let currentYear = today.getFullYear();
+// let currentmonth = today.getMonth();
+// let currentyear = today.getFullYear();
 // let selectYear = document.getElementById("year");
 // let selectMonth = document.getElementById("month");
 
-// let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-// let monthAndYear = document.getElementById("monthAndYear");
-// showCalendar(currentMonth, currentYear);
+// let monthandyear = document.getElementById("monthandyear");
+// showCalendar(currentmonth, currentyear)
+
+let date = 1;
+for (let i = 0; i< 6; i++) 
+{
+    let row = document.createElement("tr");
+}
+
+let firstday = (new Date(year, month)).getDay();
+
+//check how many days in a month
+daysinmonth(imonth, iyear)
+{
+    return 32 - new Date(iyear, imnoth, 32) .getDate();
+}
